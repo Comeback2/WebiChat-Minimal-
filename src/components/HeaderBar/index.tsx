4c050d68bd3bc747518b3917e36ed3120070b8ff
Comeback2 <156937985+Comeback2@users.noreply.github.com>
2024-01-21 21:17:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/HeaderBar/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { GithubOutlined } from '@ant-design/icons'
+import { Layout, Space, Typography } from 'antd'
+
+import styles from './index.module.less'
+
+const { Link } = Typography
+
+const { Header } = Layout
+
+const HeaderBar = () => {
+  return (
+    <>
+      <Header className={styles.header}>
+        <div className={styles.logoBar}>
+          <Link href="/">
+            <img alt="logo" src="/logo192.png" />
+            <h1>WebiChat minimalist interface </h1>
+          </Link>
+        </div>
+        <Space className={styles.right} size={0}>
+          <span className={styles.right}>
+          
+          </span>
+        </Space>
+      </Header>
+      <div className={styles.vacancy} />
+    </>
+  )
+}
+
+export default HeaderBar
